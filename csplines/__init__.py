ded977320983af279040f430b467c251a99fee0d
Gianmarco Callegher <gianmarco.callegher@uni-goettingen.de>
2025-07-15 15:48:37 +0200

    Refactor

--- csplines/__init__.py
+++ csplines/__init__.py
@@ -1,2 +1,2 @@
-from .splines import build_design_matrix_b_spline, build_p_spline_matrix, constraint_sumzero, center_spline
+from .splines import create_equidistant_knots, build_design_matrix_b_spline, build_p_spline_matrix, constraint_sumzero, center_spline
 
